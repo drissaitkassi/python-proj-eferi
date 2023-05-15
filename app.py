75c9165763f0f4d2bb532d8d6c8bd57340778783
unknown <drissaitkassi@gmail.com>
2023-05-15 08:41:50 +0000

    refactor algos works great

--- app.py
+++ app.py
@@ -52,61 +52,66 @@ allowed_choices_P2=[
 
 round_Number=0
 
-player1=input("enter player 1 :")
 
-while round_Number <=2 and (player1 in allowed_choices):
-    player2=input("enter player 2 :")
-    # game logic 
-    match player1:
-        case "F":
-            
-            if  player2 in allowed_choices_P2:
-                
-                if player2 in feuilleWinCombo:
-                    print("player 1 wins ")
-                elif player2 in feuilleLoseCombo:
-                    print("player2 wins")
-                    #player1=input("enter player 1 :")
-                else:
-                    print("Draw")
-                    #player1=input("enter player 1 :")
-
-            else:
-                print("player 2 entered an unvalid choice")
-                #round number will increment so i need to decrement it since the player
-                #entered an invalid input 
-                round_Number-=1
+while  (round_Number<3):
+    print(f"round numer is : {round_Number}")
     player1=input("enter player 1 :")
+    player2=input("enter player 2 :")
+    if player1 in allowed_choices:
+
+        # game logic 
+        match player1:
+            case "F":
                 
-                
-    #     case "S":
-    #         if player2 in sizeauxWinCombo:
-    #             print("player 1 wins ")
-    #         elif player2 in sizeauxLoseCombo:
-    #             print("player2 wins")
-    #         else:
-    #             print("Draw")
-    #     case "P":
-    #         if player2 in pierreWinCombo:
-    #             print("player 1 wins ")
-    #         elif player2 in pierreLoseCombo:
-    #             print("player2 wins")
-    #         else:
-    #             print("Draw")
-    #     case "L":
-    #         if player2 in lezardWinCombo:
-    #             print("player 1 wins ")
-    #         elif player2 in lezardLoseCombo:
-    #             print("player2 wins")
-    #         else:
-    #             print("Draw")
-    #     case "Sp":
-    #         if player2 in spockdWinCombo:
-    #             print("player 1 wins ")
-    #         elif player2 in spockdLoseCombo:
-    #             print("player2 wins")
-    #         else:
-    #             print("Draw")
-    round_Number+=1
-    print(round_Number)
+                if  player2 in allowed_choices_P2:
+                    
+                    if player2 in feuilleWinCombo:
+                        print("player 1 wins ")
+                    elif player2 in feuilleLoseCombo:
+                        print("player2 wins")
+                        #player1=input("enter player 1 :")
+                    else:
+                        print("Draw")
+                        #player1=input("enter player 1 :")
+
+                else:
+                    print("player 2 entered an unvalid choice")
+                    #round number will increment so i need to decrement it since the player
+                    #entered an invalid input 
+                    round_Number-=1
+        #player1=input("enter player 1 :")
+                    
+                    
+        #     case "S":
+        #         if player2 in sizeauxWinCombo:
+        #             print("player 1 wins ")
+        #         elif player2 in sizeauxLoseCombo:
+        #             print("player2 wins")
+        #         else:
+        #             print("Draw")
+        #     case "P":
+        #         if player2 in pierreWinCombo:
+        #             print("player 1 wins ")
+        #         elif player2 in pierreLoseCombo:
+        #             print("player2 wins")
+        #         else:
+        #             print("Draw")
+        #     case "L":
+        #         if player2 in lezardWinCombo:
+        #             print("player 1 wins ")
+        #         elif player2 in lezardLoseCombo:
+        #             print("player2 wins")
+        #         else:
+        #             print("Draw")
+        #     case "Sp":
+        #         if player2 in spockdWinCombo:
+        #             print("player 1 wins ")
+        #         elif player2 in spockdLoseCombo:
+        #             print("player2 wins")
+        #         else:
+        #             print("Draw")
+        round_Number+=1
+        
+    else:
+        break
     
